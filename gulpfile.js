f8f25abda5db8c484907100b043c7b1a7b6bf54c
анна горшкова < >
2016-08-27 01:17:24 +0700

    end

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,43 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass');
+		sass = require('gulp-sass'),
+		connect = require('gulp-connect'),
+		opn = require('opn');
+
 
 gulp.task('sass', function() {
 	return gulp.src('app/sass/main.sass')
 	.pipe(sass())
 	.pipe(gulp.dest('app/css'))
 });
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true,
+    port: 8888
+  });
+  opn('http://localhost:8888')
+});
+
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('css', function () {
+  gulp.src('./app/css/*.css')
+    .pipe(connect.reload());
+});
+
+gulp.task('js', function () {
+  gulp.src('./app/*.js')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch(['./app/css/*.css'], ['css']);
+  gulp.watch(['./app/*.js'], ['js']);
+});
+
+gulp.task('default', ['connect', 'watch']);
